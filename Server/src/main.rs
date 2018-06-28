b5602feac324e7987787678384b3d7c7200af168
Qian Linfeng <thewawar@gmail.com>
2018-06-28 20:19:10 +0800

    Basic server

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,21 @@
+extern crate actix_web;
+#[macro_use]
+extern crate serde_json;
+
+use actix_web::{server, App, HttpRequest, Responder};
+
+fn greet(req: HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", to)
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(greet))
+            .resource("/{name}", |r| r.f(greet))
+    })
+        .bind("0.0.0.0:8000")
+        .expect("Can not bind to port 8000")
+        .run();
 }
